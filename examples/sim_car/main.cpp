21bef3fc568b3a245a159871ac6b87371d4adf80
Hadi <hadi88@gmail.com>
2018-02-08 12:09:56 -0700

    updates CLF-based controller (sim_car)

--- examples/sim_car/main.cpp
+++ examples/sim_car/main.cpp
@@ -18,20 +18,201 @@
 //  commandMSG.set_throttle_percent(_msg.axes().data(5)*20);
 //}
 
-double V(double th, double x, double y, double v){
-  return 11.7355150858*th*th + 9.57145993336*x*x + 6.11955646782*y*y + 8.35433503106*v*v - 7.53735437299*th*x + 6.06073796355*th*y - 7.04427801438*th*v + 3.41207480872*x*y - 3.76334532872*v*x + 2.57409404743*v*y;
+double V(double th, double x, double y, double v, double p){
+  return 0.39831236926*p + 2.11888596261*th*th + 3.32562359169*x*x + 2.0172737766*y*y + 2.29087402521*v*v - 1.87807867872*th*x + 1.06235865938*th*y - 0.228247036325*th*v + 0.801169764975*x*y - 0.930823893073*v*x - 0.209705878867*v*y;
+}
+
+double V_pure(double th, double x, double y, double v){
+  return 2.11888596261*th*th + 3.32562359169*x*x + 2.0172737766*y*y + 2.29087402521*v*v - 1.87807867872*th*x + 1.06235865938*th*y - 0.228247036325*th*v + 0.801169764975*x*y - 0.930823893073*v*x - 0.209705878867*v*y;
 }
 
 double dV(double u_1, double u_2, double u_3, double th, double x, double y, double v, double p){
-  double th_t = th + (1.2667*p - 1.6268);
-  double sin_th = (-0.1021*th_t*th_t*th_t+0.0409*th_t*th_t+0.9522*th_t-0.0121);
-  double cos_th = (-0.0951*th_t*th_t*th_t-0.5044*th_t*th_t+0.0733*th_t+1.0073);
-  double v_t  = v  + (1.43);
-  double r = (2*11.7355150858*th - 7.53735437299*x + 6.06073796355*y - 7.04427801438*v)*(u_2-(1.2667)*(1+u_3)) +
-    (2*9.57145993336*x - 7.53735437299*th + 3.41207480872*y - 3.76334532872*v)*((v_t*-sin_th)-(-0.5435*p*2 + 1.5204)*(1+u_3)) +
-    (2*6.11955646782*y + 6.06073796355*th + 3.41207480872*x + 2.57409404743*v)*((v_t*cos_th)-(0.6758*p*2 + 0.2184)*(1+u_3)) +
-    (2*8.35433503106*v - 7.04427801438*th - 3.76334532872*x + 2.57409404743*y)*(u_1);
+  double v_r = 1.57;
+  double dth_r = 2*0.3927;
+  double v_t = v + v_r;
+  double sin_th = (-0.1569*th*th*th+0.9977*th);
+  double cos_th = (-0.4622*th*th+0.9959);
+  double r = (2*2.11888596261*th - 1.87807867872*x + 1.06235865938*y - 0.228247036325*v)*(-2.9380*v_t*u_2 - dth_r*(1+u_3)) + 
+    (2*3.32562359169*x - 1.87807867872*th + 0.801169764975*y - 0.930823893073*v)*(dth_r*(1+u_3)*y + v_t*-sin_th) + 
+    (2*2.0172737766*y + 1.06235865938*th + 0.801169764975*x - 0.209705878867*v)*(dth_r*(1+u_3)*(-x) + v_t*cos_th - v_r*(1+u_3)) + 
+    (2*2.29087402521*v - 0.228247036325*th - 0.930823893073*x - 0.209705878867*y)*(u_1);
+  double boundary = 1;
+  double L = V(th, x, y, v, p);
+  if( L > boundary )
+    r += 0.39831236926*(1+u_3);
   return r;
+}
+
+double rem(double a, double b){
+  double q = a / b;
+  return a - ((int)q)*b;
+}
+
+double rem(double a, int b){
+  double dec = a - (int) a;
+  return ((int)a % b) + dec;
+}
+
+class Input{
+  public: double u_1;
+  public: double u_2;
+  public: double u_3;
+  public: int seg;
+  public: Input(double u_1, double u_2, double u_3, int seg){
+    this->u_1 = u_1;
+    this->u_2 = u_2;
+    this->u_3 = u_3;
+    this->seg = seg;
+  }
+};
+
+Input K(double th_t, double x_t, double y_t, double v_t, double p_t, int seg_prev, double u_1_prev, double u_2_prev, double u_3_prev){
+  
+  double horizon = 2;
+  int seg = (int)(rem(p_t, horizon*4)/horizon);
+  if(seg != seg_prev)
+    std::cout << "\n\n\nsegment " << seg << "\n\n\n" << std::endl;
+
+  double p = rem(p_t, horizon);
+
+  double new_x = x_t;
+  double new_y = y_t;
+  double new_th = th_t;
+  if (seg == 1){
+      new_x = y_t;
+      new_y = -x_t;
+      new_th = th_t-(SP_PI/2);
+  }
+  if (seg == 2){
+      new_x = -x_t;
+      new_y = -y_t;
+      new_th = th_t+SP_PI;
+      // std::cout << new_th << ":" << rem((new_th+SP_PI), (2*SP_PI))-SP_PI << std::endl;
+  }
+  if (seg == 3){
+      new_x = -y_t;
+      new_y = x_t;
+      new_th = th_t+(SP_PI/2);
+  }
+  new_th = rem((new_th+SP_PI), (2*SP_PI))-SP_PI;
+
+  x_t = new_x;
+  y_t = new_y;
+  th_t = new_th;
+
+  // std::cout << "state: " << th_t << ", " << x_t << ", " << y_t << ", " << v_t << ", " << p << std::endl;
+
+  double dth_r = 2*0.3927;
+  double th_r = (dth_r*p);
+  double v_r = 1.57;
+  double x_r = 2*cos(th_r);
+  double y_r = 2*sin(th_r);
+  double th_d = th_t - th_r;
+  double v_d  = v_t  - v_r;
+  double x_d  = x_t  - x_r;
+  double y_d  = y_t  - y_r;
+  double th = th_d;
+  double v = v_d;
+  double x = cos(-th_r)*x_d-sin(-th_r)*y_d;
+  double y = sin(-th_r)*x_d+cos(-th_r)*y_d;
+
+  double L = V(th, x, y, v, p);
+  // std::cout << "L: " << L << std::endl;
+
+  double sin_th = (-0.1569*th*th*th+0.9977*th);
+  double cos_th = (-0.4622*th*th+0.9959);
+
+  double boundary = 1;
+
+  double b_1 = (2*2.29087402521*v - 0.228247036325*th - 0.930823893073*x - 0.209705878867*y);
+  double b_2 = (2*2.11888596261*th - 1.87807867872*x + 1.06235865938*y - 0.228247036325*v)*(-2.9380*(v_t));
+  double b_3 = (2*2.11888596261*th - 1.87807867872*x + 1.06235865938*y - 0.228247036325*v)*(- dth_r) + 
+    (2*3.32562359169*x - 1.87807867872*th + 0.801169764975*y - 0.930823893073*v)*(dth_r*y) + 
+    (2*2.0172737766*y + 1.06235865938*th + 0.801169764975*x - 0.209705878867*v)*(dth_r*(-x) - v_r);
+  if (L > boundary){
+    b_3 += 0.39831236926;
+  }
+  double a = (2*2.11888596261*th - 1.87807867872*x + 1.06235865938*y - 0.228247036325*v)*(-2.9380*v_t*0 - dth_r*(1+0)) + 
+    (2*3.32562359169*x - 1.87807867872*th + 0.801169764975*y - 0.930823893073*v)*(dth_r*(1+0)*y + v_t*-sin_th) + 
+    (2*2.0172737766*y + 1.06235865938*th + 0.801169764975*x - 0.209705878867*v)*(dth_r*(1+0)*(-x) + v_t*cos_th - v_r*(1+0)) + 
+    (2*2.29087402521*v - 0.228247036325*th - 0.930823893073*x - 0.209705878867*y)*(0);
+  if (L > boundary){
+    a += 0.39831236926;
+  }
+  double beta = b_1*b_1 + b_2*b_2 + b_3*b_3;
+  
+  double u1_max = 4;
+  double u1_min = -4;
+  double u2_max = 1;
+  double u2_min = -1;
+  double u3_max = 9;
+  double u3_min = -0.9;
+
+  double beta_low = 0.1/2;
+  double u_1 = 0;
+  double u_2 = 0;
+  double u_3 = 0;
+
+  double dL = 0;
+  double Lpure = V_pure(th, x, y, v);
+  if (beta < beta_low){ // use default u if L is small
+    u_1 = 0;
+    u_2 = 0;
+    u_3 = 0;
+    dL = dV(u_1, u_2, u_3, th, x, y, v, p);
+  }else if (beta < 2*beta_low && u_1_prev == 0 && u_2_prev == 0 && u_3_prev == 0){ // continue using default u
+    u_1 = u_1_prev;
+    u_2 = u_2_prev;
+    u_3 = u_3_prev;
+    dL = dV(u_1, u_2, u_3, th, x, y, v, p);
+  }else if (Lpure < 0.1/2){
+    u_1 = 0;
+    u_2 = dth_r/(-2.9380*v_r);
+    u_3 = 0;
+    dL = dV(u_1, u_2, u_3, th, x, y, v, p);
+  }else{
+    double phi = 0;
+    double dec_rate = 0;
+    if (beta > beta_low){ // Sontage formula
+      dec_rate = (0.5*sqrt(a*a+beta)+a)/beta;
+      phi = (dec_rate+a)/beta;
+      u_1 = -b_1*phi;
+      u_2 = -b_2*phi;
+      u_3 = -b_3*phi;
+      u_1 = (u_1>u1_max)?u1_max:((u_1<u1_min)?u1_min:u_1);
+      u_2 = (u_2>u2_max)?u2_max:((u_2<u2_min)?u2_min:u_2);
+      u_3 = (u_3>u3_max)?u3_max:((u_3<u3_min)?u3_min:u_3);
+    }
+    dL = dV(u_1, u_2, u_3, th, x, y, v, p);
+
+    double thresh = -0.1*L;
+    double coef = 1.1;
+    while (dL > thresh){ // increase decrease rate size if L is not decreasing
+      if(phi == 0 || beta < beta_low)
+        break;
+      dec_rate = coef*dec_rate;
+      phi = (dec_rate + a)/beta;
+      double u_1t = -b_1*phi; double u_2t = -b_2*phi; double u_3t = -b_3*phi;
+      u_1t = (u_1t>u1_max)?u1_max:((u_1t<u1_min)?u1_min:u_1t);
+      u_2t = (u_2t>u2_max)?u2_max:((u_2t<u2_min)?u2_min:u_2t);
+      u_3t = (u_3t>u3_max)?u3_max:((u_3t<u3_min)?u3_min:u_3t);
+      // std::cout << ";" << dec_rate << ", " << phi << ":" << u_1t << ", " << u_2t << ", " << u_3t << std::endl;
+      double dL_t = dV(u_1t, u_2t, u_3t, th, x, y, v, p);
+      if (dL_t > dL){
+        if (L > boundary){
+          std::cout << "error: dL increases... V:" << L << ", Beta:" << beta << std::endl;
+        }
+        break;
+      }
+      dL = dL_t; u_1 = u_1t; u_2 = u_2t; u_3 = u_3t;
+      if ((u_1 == u1_min || u_1 == u1_max) && (u_2 == u2_min || u_2 == u2_max)  && (u_3 == u3_min || u_3 == u3_max)){
+        break;
+      }
+    }
+    // if (dL > thresh)
+    //   std::cout << "dL :" << dL << std::endl;
+  }
+  return Input(u_1, u_2, u_3, seg);
 }
 
 int main(int argc, char** argv) {
@@ -84,7 +265,7 @@ int main(int argc, char** argv) {
   double v0_x = -v0*sin(th0);
   double v0_y = v0*cos(th0);
 
-  std::cout << "state: " << th0 << ", " << x0 << ", " << y0 << ", " << v0_x << ", " << v0_y << ", " << torque0 << ", " << turn0 << std::endl;
+  // std::cout << "state: " << th0 << ", " << x0 << ", " << y0 << ", " << v0_x << ", " << v0_y << ", " << torque0 << ", " << turn0 << std::endl;
   
 // int first_time = 1;
 while(1){
@@ -97,15 +278,16 @@ while(1){
 
   car.SetState(initState);
 
-  int i = 0;
 
-  double p = p0;
+  double p_t = p0;
   double u_1_prev = torque0;
   double u_2_prev = turn0;
   double u_3_prev = dp0;
 
   double tau = 0.01;
-  while(p < 1) {
+
+  int seg_prev = 0;
+  while(p_t < 64) {
 //    car.SetEngineMaxVel(commandMSG.throttle_percent());
 //    car.SetFrontSteeringAngle(commandMSG.steering_angle());
 
@@ -117,169 +299,35 @@ while(1){
     double y_t = car.GetState().pose.translation()[1];
     double th_t = std::atan2(rotmat(1,0),rotmat(0,0));
     double v_t = car.GetState().linvel.norm();
-    p += (1+u_3_prev)*tau;
+    p_t += (1+u_3_prev)*tau;
 
-    std::cout << "state: " << th_t << ", " << x_t << ", " << y_t << ", " << v_t << ", " << p << std::endl;
-
-    double th = th_t - (1.2667*p - 1.6268);
-    double x  = x_t  - (-0.5435*p*p + 1.5204*p - 1.0035);
-    double y  = y_t  - (0.6758*p*p + 0.2184*p - 1.0164);
-    double v  = v_t  - (1.43);
-
-    double L = V(th, x, y, v);
-    std::cout << "L: " << L << std::endl;
-
-    double sin_th = (-0.1021*th_t*th_t*th_t+0.0409*th_t*th_t+0.9522*th_t-0.0121);
-    double cos_th = (-0.0951*th_t*th_t*th_t-0.5044*th_t*th_t+0.0733*th_t+1.0073);
-
-    double b_1 = (2*8.35433503106*v - 7.04427801438*th - 3.76334532872*x + 2.57409404743*y);
-    double b_2 = (2*11.7355150858*th - 7.53735437299*x + 6.06073796355*y - 7.04427801438*v);
-    double b_3 = (2*11.7355150858*th - 7.53735437299*x + 6.06073796355*y - 7.04427801438*v)*(0-(1.2667)) +
-    (2*9.57145993336*x - 7.53735437299*th + 3.41207480872*y - 3.76334532872*v)*(0-(-0.5435*p*2 + 1.5204)) +
-    (2*6.11955646782*y + 6.06073796355*th + 3.41207480872*x + 2.57409404743*v)*(0-(0.6758*p*2 + 0.2184)) +
-    (2*8.35433503106*v - 7.04427801438*th - 3.76334532872*x + 2.57409404743*y)*(0);
-    double a = (2*11.7355150858*th - 7.53735437299*x + 6.06073796355*y - 7.04427801438*v)*(0-(1.2667)*(1+0)) +
-    (2*9.57145993336*x - 7.53735437299*th + 3.41207480872*y - 3.76334532872*v)*((v_t*-sin_th)-(-0.5435*p*2 + 1.5204)*(1+0)) +
-    (2*6.11955646782*y + 6.06073796355*th + 3.41207480872*x + 2.57409404743*v)*((v_t*cos_th)-(0.6758*p*2 + 0.2184)*(1+0)) +
-    (2*8.35433503106*v - 7.04427801438*th - 3.76334532872*x + 2.57409404743*y)*(0);
-
-    double beta = b_1*b_1 + b_2*b_2 + b_3*b_3;
-
-    double beta_low = 0.001;
-    double u_1 = 0;
-    double u_2 = 0;
-    double u_3 = 0;
-
-    double u1_max = 4;
-    double u1_min = -4;
-    double u2_max = 2.45;
-    double u2_min = -2.45;
-    double u3_max = 9;
-    double u3_min = -0.9;
     
+    Input feedback = K(th_t, x_t, y_t, v_t, p_t, seg_prev, u_1_prev, u_2_prev, u_3_prev);
 
-    double dL = 0;
-    if (L < 0.1){ // use default u if L is small
-      u_1 = 0;
-      u_2 = 1.2667;
-      u_3 = 0;
-      dL = dV(u_1, u_2, u_3, th, x, y, v, p);
-    }else if (L < 0.2 && u_1_prev == 0 && u_2_prev == -1.2667 && u_3_prev == 0){ // continue using default u
-      u_1 = u_1_prev;
-      u_2 = u_2_prev;
-      u_3 = u_3_prev;
-      dL = dV(u_1, u_2, u_3, th, x, y, v, p);
-    }else{
-      double phi = 0;
-      double dec_rate = 0;
-      if (beta > beta_low){ // Sontage formula
-        if (beta > 5){
-          dec_rate = 0.1*sqrt(a*a+beta*beta*beta);
-        } else if (beta > 0.5){
-          dec_rate = 0.1*sqrt(a*a+beta*beta);
-        }else{
-          dec_rate = 0.01*sqrt(a*a+beta);
-        }
-        phi = (dec_rate+a)/beta;
-        u_1 = -b_1*phi;
-        u_2 = -b_2*phi;
-        u_3 = -b_3*phi;
-        u_1 = (u_1>u1_max)?u1_max:((u_1<u1_min)?u1_min:u_1);
-        u_2 = (u_2>u2_max)?u2_max:((u_2<u2_min)?u2_min:u_2);
-        u_3 = (u_3>u3_max)?u3_max:((u_3<u3_min)?u3_min:u_3);
-      }
-      dL = dV(u_1, u_2, u_3, th, x, y, v, p);
-
-      double thresh = -0.1*L;
-      double coef = 1.1;
-      while (dL > thresh){ // increase decrease rate size if L is not decreasing
-        if(phi == 0 || beta < beta_low)
-          break;
-        dec_rate = coef*dec_rate;
-        phi = (dec_rate + a)/beta;
-        double u_1t = -b_1*phi; double u_2t = -b_2*phi; double u_3t = -b_3*phi;
-        u_1t = (u_1t>u1_max)?u1_max:((u_1t<u1_min)?u1_min:u_1t);
-        u_2t = (u_2t>u2_max)?u2_max:((u_2t<u2_min)?u2_min:u_2t);
-        u_3t = (u_3t>u3_max)?u3_max:((u_3t<u3_min)?u3_min:u_3t);
-        // std::cout << ";" << dec_rate << ", " << phi << ":" << u_1t << ", " << u_2t << ", " << u_3t << std::endl;
-        double dL_t = dV(u_1t, u_2t, u_3t, th, x, y, v, p);
-        if (dL_t > dL){
-          std::cout << "error: dL increases... V:" << L << ", Beta:" << beta << std::endl;
-          // std::cout << "\t " << u_1 << ", " << u_2 << ", " << u_3;
-          // std::cout << " : " << u_1t<< ", " << u_2t<< ", " << u_3t << std::endl;
-          // std::cout << "\t " << dL << " : " << dL_t << " : " << (a + u_1t*b_1 + u_2t*b_2 + u_3t*b_3) << std::endl; 
-          // std::cout << "\t" << b_1 << ", " << b_2 << ", " << b_3 << ", " << phi << std::endl;
-          break;
-        }
-        dL = dL_t; u_1 = u_1t; u_2 = u_2t; u_3 = u_3t;
-        if ((u_1 == u1_min || u_1 == u1_max) && (u_2 == u2_min || u_2 == u2_max)  && (u_3 == u3_min || u_3 == u3_max)){
-          break;
-        }
-        // std::cout << ":" << u_1 << ", " << u_2 << ", " << u_3 << std::endl;
-      }
-      if (dL > thresh)
-        std::cout << "dL :" << dL << std::endl;
-    }
-
+    double u_1 = feedback.u_1;
+    double u_2 = feedback.u_2;
+    double u_3 = feedback.u_3;
+    int seg = feedback.seg;
 
     u_1_prev = u_1;
     u_2_prev = u_2;
     u_3_prev = u_3;
-    // calculate output
-
+    seg_prev = seg;
+    
     double torque = (u_1-0.3124)/13908;
-    // double turn = u_2/-3.5266;
-    // if (turn > 0.5){
-    //   double torque_adj = 0.005*turn*turn;
-    //   std::cout << "torque adj: " << torque_adj << std::endl;
-    //   torque += ((torque>0)?1:-1)*torque_adj;
-    // }
-    // torque = (torque > 0.001)?0.001:((torque<-0.001)?-0.001: torque);
     torque += 0.0002;
-
-    // u_2 = -0.067731*turn -0.026598*v -2.9128*(turn*v) -0.077826*(turn*turn) + 0.011698*(v*v);
-    double quad_a = -0.077826;
-    double quad_b = -0.067731 -2.9128*v_t;
-    double quad_c = -0.026598*v_t + 0.011698*(v_t*v_t) - (u_2);
-    double quad_root1 = (-quad_b + sqrt(quad_b*quad_b - 4*quad_a*quad_c))/(2*quad_a);
-    double quad_root2 = (-quad_b - sqrt(quad_b*quad_b - 4*quad_a*quad_c))/(2*quad_a);
-    u2_max = u2_max/3.5266;
-    u2_min = u2_min/3.5266;
-    quad_root1 = (quad_root1>u2_max)?u2_max:((quad_root1<u2_min)?u2_min:quad_root1);
-    quad_root2 = (quad_root2>u2_max)?u2_max:((quad_root2<u2_min)?u2_min:quad_root2);
-    double turn = u_2/-3.5266;
-    if (quad_root1 < u2_max && quad_root1 > u2_min && abs(quad_root1 - turn) < abs(quad_root2 - turn)){
-      turn = quad_root1;
-    }else if (quad_root2 < u2_max && quad_root2 > u2_min && abs(quad_root2 - turn) < abs(quad_root1 - turn)){
-      turn = quad_root2;
-    }
-    std::cout << "turn: " << quad_root1 << " , " << quad_root2 << " , " << (u_2/-3.5266) << std::endl;
-    // double turn = quad_root1;
-    // std::cout << "turn: " << quad_root1 << " , " << quad_root2 << " , " << (u_2/-3.5266) << std::endl;
-    // if(abs(quad_root1 - (u_2/-3.5266)) > abs(quad_root2 - (u_2/-3.5266))){
-    //   turn = quad_root2;
-    // }
-    // if (abs(turn - (u_2/-3.5266)) > 0.5 ){
-    //   turn = u_2/-3.5266;
-    // }
-    // if (u_1*u_1 < 0.00005*0.00005){
-    //   turn *= 2;
-    // }
-    // double turn_adj = 0.01*u_1*u_1;
-    // turn += ((turn>0)?1:-1)*turn_adj;
-    // if (turn > 0.8) turn = 0.8;
-    // if (turn < -0.8) turn = -0.8;
+    double turn = atan(u_2);
+    
     car.SetEngineMaxVel(100);
     car.SetEngineTorque(torque);
     car.SetFrontSteeringAngle(turn);
-    
-    std::cout << "control: " << torque << ", " << turn << std::endl;
+    // std::cout << "u: " << u_1 << ", " << u_2 << std::endl;
+    // std::cout << "control: " << torque << ", " << turn << std::endl;
 
     spworld.objects_.StepPhySimulation(tau);
 
     spworld.gui_.Iterate(spworld.objects_);
-    std::this_thread::sleep_for(std::chrono::nanoseconds((int)(tau*1000000*10)));
-    i++;
+    std::this_thread::sleep_for(std::chrono::milliseconds((int)(tau*1000*(1))));
   }
 }
   return 0;
