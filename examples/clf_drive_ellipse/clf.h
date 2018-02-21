169666c916740e6ded56499bd1ac28d40bf86ad5
Hadi <hadi88@gmail.com>
2018-02-21 14:55:55 -0700

    adds controller for ellipse curve

--- examples/clf_drive_ellipse/clf.h
+++ examples/clf_drive_ellipse/clf.h
@@ -2,27 +2,28 @@
 #define CLF_H__
 
 double V(double th, double x, double y, double v, double p){
-  return 0.279029300741*p + 1.83588960532*th*th + 3.43974329742*x*x + 3.47312955982*y*y + 1.62793673047*v*v -1.52266178012*th*x -0.500924627272*th*y -0.506690924341*th*v -0.256212179064*x*y + 0.504234712498*v*x + 1.7872439758*v*y;
+  //path following:
+  return 0.251836191408*p + 2.31106217689*th*th + 3.23504654641*x*x + 3.28654452988*y*y + 1.77745630912*v*v - 1.03187637766*th*x - 0.521359195573*th*y - 0.161523642071*th*v + 0.825082067471*x*y - 0.914355165442*v*x + 0.0894413885605*v*y;
 }
 
 double V_pure(double th, double x, double y, double v){
-  return 1.83588960532*th*th + 3.43974329742*x*x + 3.47312955982*y*y + 1.62793673047*v*v -1.52266178012*th*x -0.500924627272*th*y -0.506690924341*th*v -0.256212179064*x*y + 0.504234712498*v*x + 1.7872439758*v*y;
+  return 2.31106217689*th*th + 3.23504654641*x*x + 3.28654452988*y*y + 1.77745630912*v*v - 1.03187637766*th*x - 0.521359195573*th*y - 0.161523642071*th*v + 0.825082067471*x*y - 0.914355165442*v*x + 0.0894413885605*v*y;
 }
 
 double dV(double u_1, double u_2, double u_3, double th, double x, double y, double v, double p){
   double v_r = 2;
-  double dth_r = 0;
+  double dth_r = (3.1416/11.0437848)*(0.8+1.2*(3*(p-1.53)*(p-1.53)));
   double v_t = v + v_r;
   double sin_th = (-0.1569*th*th*th+0.9977*th);
   double cos_th = (-0.4622*th*th+0.9959);
-  double r = (2*1.83588960532*th -1.52266178012*x -0.500924627272 *y -0.506690924341*v)*(-2.9380*v_t*u_2 - dth_r*(1+u_3)) + 
-     (2*3.43974329742*x -1.52266178012*th -0.256212179064*y + 0.504234712498 *v)*(dth_r*(1+u_3)*y + v_t*-sin_th) + 
-     (2*3.47312955982*y -0.500924627272 *th -0.256212179064*x + 1.7872439758*v)*(dth_r*(1+u_3)*(-x) + v_t*cos_th - v_r*(1+u_3)) + 
-     (2*1.62793673047*v -0.506690924341*th + 0.504234712498 *x + 1.7872439758*y)*(u_1);
+  double r = (2*2.31106217689*th - 1.03187637766*x - 0.521359195573*y - 0.161523642071*v)*(-2.9380*v_t*u_2 - dth_r*(1+u_3)) + 
+    (2*3.23504654641*x - 1.03187637766*th + 0.825082067471*y - 0.914355165442*v)*(dth_r*(1+u_3)*y + v_t*-sin_th) + 
+    (2*3.28654452988*y - 0.521359195573*th + 0.825082067471*x + 0.0894413885605*v)*(dth_r*(1+u_3)*(-x) + v_t*cos_th - v_r*(1+u_3)) + 
+    (2*1.77745630912*v - 0.161523642071*th - 0.914355165442*x + 0.0894413885605*y)*(u_1);
   double boundary = 1;
   double L = V(th, x, y, v, p);
   if( L > boundary )
-    r += 0.279029300741*(1+u_3);
+    r += 0.251836191408*(1+u_3);
   return r;
 }
 
@@ -51,34 +52,22 @@ class Input{
 
 Input K(double th_t, double x_t, double y_t, double v_t, double p_t, int seg_prev, double u_1_prev, double u_2_prev, double u_3_prev){
   
-  // double horizon = 1.5;
-  // int seg = (int)(rem(p_t, horizon*4)/horizon);
-  // if(seg != seg_prev)
-  //   std::cout << "\n\n\nsegment " << seg << "\n\n\n" << std::endl;
-  int seg = seg_prev;
+  double horizon = 3.06;
+  int seg = (int)(rem(p_t, horizon*2)/horizon);
+  if(seg != seg_prev)
+    std::cout << "\n\n\nsegment " << seg << "\n\n\n" << std::endl;
 
-  double p = p_t;//rem(p_t, horizon);
+  double p = rem(p_t, horizon);
 
   double new_x = x_t;
   double new_y = y_t;
   double new_th = th_t;
-  // if (seg == 1){
-  //     new_x = y_t;
-  //     new_y = -x_t;
-  //     new_th = th_t-(SP_PI/2);
-  // }
-  // if (seg == 2){
-  //     new_x = -x_t;
-  //     new_y = -y_t;
-  //     new_th = th_t+SP_PI;
-  //     // std::cout << new_th << ":" << rem((new_th+SP_PI), (2*SP_PI))-SP_PI << std::endl;
-  // }
-  // if (seg == 3){
-  //     new_x = -y_t;
-  //     new_y = x_t;
-  //     new_th = th_t+(SP_PI/2);
-  // }
-  new_th = rem((new_th+SP_PI), (2*SP_PI))-SP_PI;
+  if (seg == 1){
+      new_x = -x_t;
+      new_y = -y_t;
+      new_th = th_t-(SP_PI);
+  }
+  new_th = rem((new_th+3*SP_PI), (2*SP_PI))-SP_PI;
 
   x_t = new_x;
   y_t = new_y;
@@ -86,11 +75,13 @@ Input K(double th_t, double x_t, double y_t, double v_t, double p_t, int seg_pre
 
   // std::cout << "state: " << th_t << ", " << x_t << ", " << y_t << ", " << v_t << ", " << p << std::endl;
 
-  double dth_r = 0;
-  double th_r = 0 - SP_PI/2;
+  double dth_r = (3.1416/11.0437848)*(0.8+1.2*(3*(p-1.53)*(p-1.53)));
+  double th_r = rem((((3.1416/11.0437848)*(0.8*p+1.2*(3.581577+(p-1.53)*(p-1.53)*(p-1.53))))+SP_PI), (2*SP_PI))-SP_PI;
   double v_r = 2;
-  double x_r = -4 + 2*p;
-  double y_r = 0;
+  double p2 = p*p;
+  double p3 = p2*p;
+  double x_r = 0.0610*p2*p3   -0.4666*p3*p    +1.4088*p3   -2.0969*p2   -0.0255*p    +1.9524;
+  double y_r = -0.0887*p3*p    +0.5428*p3   -1.4705*p2    +1.9585*p   -0.0256;
   double th_d = th_t - th_r;
   double v_d  = v_t  - v_r;
   double x_d  = x_t  - x_r;
@@ -99,30 +90,30 @@ Input K(double th_t, double x_t, double y_t, double v_t, double p_t, int seg_pre
   double v = v_d;
   double x = cos(-th_r)*x_d-sin(-th_r)*y_d;
   double y = sin(-th_r)*x_d+cos(-th_r)*y_d;
-
+  double Lpure = V_pure(th, x, y, v);
   double L = V(th, x, y, v, p);
-  std::cout << "L: " << L << std::endl;
+  std::cout << "L: " << Lpure << std::endl;
 
   double sin_th = (-0.1569*th*th*th+0.9977*th);
   double cos_th = (-0.4622*th*th+0.9959);
 
   double boundary = 1;
 
-  double  b_1 = (2*1.62793673047*v -0.506690924341*th + 0.504234712498 *x + 1.7872439758*y);
-  double  b_2 = (2*1.83588960532*th -1.52266178012*x -0.500924627272 *y -0.506690924341*v)*(-2.9380*(v_t));
-  double  b_3 = (2*1.83588960532*th -1.52266178012*x -0.500924627272 *y -0.506690924341*v)*(-2.9380*v_t*0 - dth_r*(1+0)) + 
-     (2*3.43974329742*x -1.52266178012*th -0.256212179064*y + 0.504234712498 *v)*(dth_r*(1+0)*y) + 
-     (2*3.47312955982*y -0.500924627272 *th -0.256212179064*x + 1.7872439758*v)*(dth_r*(1+0)*(-x) - v_r*(1+0)) + 
-     (2*1.62793673047*v -0.506690924341*th + 0.504234712498 *x + 1.7872439758*y)*(0)
+  double b_1 = (2*1.77745630912*v - 0.161523642071*th - 0.914355165442*x + 0.0894413885605*y);
+  double b_2 = (2*2.31106217689*th - 1.03187637766*x - 0.521359195573*y - 0.161523642071*v)*(-2.9380*v_t);
+  double b_3 = (2*2.31106217689*th - 1.03187637766*x - 0.521359195573*y - 0.161523642071*v)*(-2.9380*v_t*0 - dth_r*(1+0)) + 
+    (2*3.23504654641*x - 1.03187637766*th + 0.825082067471*y - 0.914355165442*v)*(dth_r*(1+0)*y) + 
+    (2*3.28654452988*y - 0.521359195573*th + 0.825082067471*x + 0.0894413885605*v)*(dth_r*(1+0)*(-x) - v_r*(1+0)) + 
+    (2*1.77745630912*v - 0.161523642071*th - 0.914355165442*x + 0.0894413885605*y)*(0);
   if (L > boundary){
-    b_3 += 0.279029300741;
+    b_3 += 0.251836191408;
   }
-  double a = (2*1.83588960532*th -1.52266178012*x -0.500924627272 *y -0.506690924341*v)*(-2.9380*v_t*0 - dth_r*(1+0)) + 
-     (2*3.43974329742*x -1.52266178012*th -0.256212179064*y + 0.504234712498 *v)*(dth_r*(1+0)*y + v_t*-sin_th) + 
-     (2*3.47312955982*y -0.500924627272 *th -0.256212179064*x + 1.7872439758*v)*(dth_r*(1+0)*(-x) + v_t*cos_th - v_r*(1+0)) + 
-     (2*1.62793673047*v -0.506690924341*th + 0.504234712498 *x + 1.7872439758*y)*(0);
+  double a = (2*2.31106217689*th - 1.03187637766*x - 0.521359195573*y - 0.161523642071*v)*(-2.9380*v_t*0 - dth_r*(1+0)) + 
+    (2*3.23504654641*x - 1.03187637766*th + 0.825082067471*y - 0.914355165442*v)*(dth_r*(1+0)*y + v_t*-sin_th) + 
+    (2*3.28654452988*y - 0.521359195573*th + 0.825082067471*x + 0.0894413885605*v)*(dth_r*(1+0)*(-x) + v_t*cos_th - v_r*(1+0)) + 
+    (2*1.77745630912*v - 0.161523642071*th - 0.914355165442*x + 0.0894413885605*y)*(0);
   if (L > boundary){
-    a += 0.279029300741;
+    a += 0.251836191408;
   }
   double beta = b_1*b_1 + b_2*b_2 + b_3*b_3;
   
@@ -130,8 +121,8 @@ Input K(double th_t, double x_t, double y_t, double v_t, double p_t, int seg_pre
   double u1_min = -4;
   double u2_max = 1;
   double u2_min = -1;
-  double u3_max = 0;
-  double u3_min = -0.0;
+  double u3_max = 9;
+  double u3_min = -0.9;
 
   double beta_low = 0.1/2;
   double u_1 = 0;
@@ -139,7 +130,7 @@ Input K(double th_t, double x_t, double y_t, double v_t, double p_t, int seg_pre
   double u_3 = 0;
 
   double dL = 0;
-  double Lpure = V_pure(th, x, y, v);
+  
   if (beta < beta_low){ // use default u if L is small
     u_1 = 0;
     u_2 = 0;
@@ -150,7 +141,7 @@ Input K(double th_t, double x_t, double y_t, double v_t, double p_t, int seg_pre
     u_2 = u_2_prev;
     u_3 = u_3_prev;
     dL = dV(u_1, u_2, u_3, th, x, y, v, p);
-  }else if (Lpure < 1){
+  }else if (Lpure < 0.5){
     u_1 = 0;
     u_2 = dth_r/(-2.9380*v_r);
     u_3 = 0;
@@ -170,7 +161,7 @@ Input K(double th_t, double x_t, double y_t, double v_t, double p_t, int seg_pre
     }
     dL = dV(u_1, u_2, u_3, th, x, y, v, p);
 
-    double thresh = -0.5*L;
+    double thresh = -0.1*L;
     double coef = 1.1;
     while (dL > thresh){ // increase decrease rate size if L is not decreasing
       if(phi == 0 || beta < beta_low)
@@ -199,4 +190,5 @@ Input K(double th_t, double x_t, double y_t, double v_t, double p_t, int seg_pre
   }
   return Input(u_1, u_2, u_3, seg);
 }
+
 #endif
