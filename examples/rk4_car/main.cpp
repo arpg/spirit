d34d5fd8954817dc5c3df3659d818cd3dc14375b
sinaaghli <sina.aghli@gmail.com>
2018-03-27 15:26:31 -0600

    added rk4_car with example carODE file

--- examples/rk4_car/main.cpp
+++ examples/rk4_car/main.cpp
@@ -3,23 +3,23 @@
 #include <spirit/spirit.h>
 #include <eigen3/Eigen/Eigen>
 #include <math.h>
+#include "CarODE.h"
 
-Eigen::ArrayXd TestODE(Eigen::VectorXd y_t) {
-  Eigen::VectorXd y_dot(y_t);
-//////////////////////
 
-//////////////////////
-  return y_dot;
-}
 
 int main(int argc, char** argv) {
 
   RK4 rk4solver;
-  rk4solver.RegisterODE(&TestODE);
+  rk4solver.RegisterODE(&CarODE2);
 
-  Eigen::ArrayXd init(2);
+  Eigen::ArrayXd init(7);
   init[0] = 0.0;
-  init[1] = 2.0;
+  init[1] = 0.0;
+  init[2] = 0.0;
+  init[3] = 0.0;
+  init[4] = 0.0;
+  init[5] = 0.0;
+  init[6] = 0.0;
 
 //  Eigen::VectorXd y_dot = TestODE(init);
 //  std::cout << "dot " << y_dot << std::endl;
@@ -29,8 +29,11 @@ int main(int argc, char** argv) {
   Eigen::ArrayXXd traj = rk4solver.Solve(init,10);
   std::cout << "traj cols " << traj.cols() << std::endl;
   std::cout << "traj rows " << traj.rows() << std::endl;
-  std::cout << traj.row(0).transpose() << std::endl;
+  std::cout << traj.row(2).transpose() << std::endl;
 
   std::cout << "Done ... !" << std::endl;
+
+
+
   return 0;
 }
