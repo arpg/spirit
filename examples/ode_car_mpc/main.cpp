c496a45a348e5435459c2149af3575251849c474
Sina Aghli <sina.aghli@gmail.com>
2018-07-25 10:49:01 -0600

    added optitrack state reading to ode_car_mpc example

--- examples/ode_car_mpc/main.cpp
+++ examples/ode_car_mpc/main.cpp
@@ -1,127 +1,61 @@
 #include <thread>
 #include <spirit/spirit.h>
 #include <atomic>
+#include <iostream>
+#include <iomanip>
+#include <HAL/Posys/PosysDevice.h>
 
-//#include <spirit/CarSimFunctorRK4.h>
+struct OptiState{
+  spPose prev_pose;
+  spPose pose;
+  spLinVel linvel;
+  spRotVel rotvel;
+  bool initialized = false;
+} optistate_;
+
+void optitrack_pose_handler(hal::PoseMsg& PoseData) {
+  // get x-y translation
+  optistate_.pose = spPose::Identity();
+  optistate_.pose.translate(spTranslation(PoseData.pose().data(0),PoseData.pose().data(1),0));
+
+  // get yaw rotation
+  spRotation full_rot(PoseData.pose().data(6),PoseData.pose().data(3),PoseData.pose().data(4),PoseData.pose().data(5));
+  Eigen::AngleAxisd yaw_rot(full_rot.EulerAngles(0,1,2)[3],Eigen::Vector3d::UnitZ());
+  optistate_.pose.rotate(yaw_rot);
+
+  // Calculate Velocities
+  if(optistate_.initialized){
+    double inv_time_dif = 1/(PoseData.device_time()-prev_pose_.device_time());
+    optistate_.linvel[0] = inv_time_dif*(optistate_.pose.translation()[0]-optistate_.prev_pose.translation()[0]);
+    optistate_.linvel[1] = inv_time_dif*(optistate_.pose.translation()[1]-optistate_.prev_pose.translation()[1]);
+    optistate_.linvel[2] = 0;
+    prev_pose_.rotvel[0] = 0;
+    prev_pose_.rotvel[1] = 0;
+    double yaw_diff = (optistate_.pose.rotation().EulerAngles(0,1,2)[2]-optistate_.prev_pose.rotation().EulerAngles(0,1,2)[2]);
+    if(yaw_diff>SP_PI){
+      yaw_diff = 2*SP_PI-yaw_diff;
+    } else if(yaw_diff<-SP_PI){
+      yaw_diff = -2*SP_PI-yaw_diff;
+    }
+    prev_pose_.rotvel[2] = inv_time_dif*yaw_diff;
+  } else {
+    linvel_ = spLinVel(0,0,0);
+    rotvel_ = spRotVel(0,0,0);
+  }
+
+  // system time vs Device time diff
+  optistate_.prev_pose = optistate_.pose;
+
+  optistate_.initialized = true;
+}
 
 int main(int argc, char** argv) {
-  spSettings settings_obj;
-  settings_obj.SetGuiType(spGuiType::GUI_PANGOSCENEGRAPH);
-  settings_obj.SetPhysicsEngineType(spPhyEngineType::PHY_BULLET);
-  spirit spworld(settings_obj);
 
-  spObjectHandle car_handle = spworld.objects_.CreateVehicle(spworld.car_param);
-  spworld.gui_.AddObject(spworld.objects_.GetObject(car_handle));
-  spAWSDCar& car = (spAWSDCar&) spworld.objects_.GetObject(car_handle);
-  // create a flat ground
-  spPose gnd_pose_ = spPose::Identity();
-  gnd_pose_.translate(spTranslation(0,0,-0.5));
-  spObjectHandle gnd_handle = spworld.objects_.CreateBox(gnd_pose_,spBoxSize(50,50,1),0,spColor(0,1,0));
-  spworld.gui_.AddObject(spworld.objects_.GetObject(gnd_handle));
+  ////////////////////////////////////////////////////////////
+  hal::Posys vicon("vicon://tracker:[dummy]");
+  vicon.RegisterPosysDataCallback(&optitrack_pose_handler);
+  ////////////////////////////////////////////////////////////
 
-  spState state;
-  state.pose = spPose::Identity();
-  state.pose.translate(spTranslation(2,0,0));
-  Eigen::AngleAxisd rot1(3*SP_PI_QUART,Eigen::Vector3d::UnitZ());
-  state.pose.rotate(rot1);
-  state.linvel = spLinVel(0.0,0,0);
-  state.rotvel = spRotVel(0,0,0);
-  state.wheel_speeds = spWheelSpeedVec(0,0,0,0);
-
-  car.SetState(state);
-
-  /////////////////////////////////
-
-  // set driving car's first pose
-
-  // create a MPC controller with horizon
-  float horizon = 5;
-  spMPC mpc(spworld.car_param,horizon);
-
-  spCtrlPts2ord_2dof controls;
-  controls.col(0) = Eigen::Vector2d(0,0);
-  controls.col(1) = Eigen::Vector2d(0,0);
-  controls.col(2) = Eigen::Vector2d(0,0);
-
-  CarSimFunctorRK4 mysim(spworld.car_param,state);
-
-int cnt = 0;
-  /*while(1)*/{
-//    car.SetPose(posys_);
-//    spPose ps = car.GetPose();
-//    ps.translation()[2] = 0.07;
-//    car.SetPose(car_init_pose);
-//    car.SetState(sst);
-
-    spTimestamp t0 = spGeneralTools::Tick();
-    if(mpc.CircleManReg(state,controls,2,3)) {
-      double calc_time = spGeneralTools::Tock_ms(t0);
-      std::cout << "controls -> \n" << controls << std::endl;
-
-      std::shared_ptr<spStateSeries> series = std::make_shared<spStateSeries>();
-      mysim(0,(int)(horizon/DISCRETIZATION_STEP_SIZE),DISCRETIZATION_STEP_SIZE,controls,0,-1,series,std::make_shared<spState>(state));
-      while(1){
-      for(int ii=0; ii<(int)(horizon/DISCRETIZATION_STEP_SIZE); ii++){
-        spState tstate(*((*series)[ii]));
-        car.SetState(tstate);
-        spworld.gui_.Iterate(spworld.objects_);
-        std::cout << "speed -> " << tstate.linvel.norm() << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-      }
-      std::cout << "done " << std::endl;
-        spworld.gui_.Iterate(spworld.objects_);
-      }
-
-//      if(cnt<3){
-//        cnt++;
-//      } else {
-//        break;
-//      }
-//      spCurve controls_curve(2,2);
-//      spPointXd next_control(2);
-//      calc_time /= 1000;
-//      spworld.objects_.StepPhySimulation(calc_time);
-//      spworld.gui_.Iterate(spworld.objects_);
-//      controls_curve.SetBezierControlPoints(controls);
-      // only get the control signal for next point and apply to car
-//      controls_curve.GetPoint(next_control,calc_time/horizon);
-//      std::cout << "next signals are " << std::fixed << std::setprecision(3) << next_control.transpose() << std::endl;
-//      car.SetFrontSteeringAngle(next_control[0]);
-//      car.SetEngineMaxVel(/*next_control[1]*/100);
-//      car.SetEngineTorque(next_control[1]*0.00001);
-//      car.SetEngineTorque(2*0.00001);
-//      if(flag_auto) {
-//          if(next_control[1] > 30) {
-//              next_control[1] = 30;
-//          }
-//          if(next_control[1] < -30) {
-//              next_control[1] = -30;
-//          }
-//          commandMSG.set_steering_angle(-next_control[0]);
-//          commandMSG.set_throttle_percent(gamepad_throttle/*next_control[1]*/);
-//      } else {
-//          commandMSG.set_steering_angle(gamepad_steering);
-//          commandMSG.set_throttle_percent(gamepad_throttle);
-//      }
-
-//      ninja_car.UpdateCarCommand(commandMSG);
-//      controls.col(0) = next_control;
-      //double calc_time = spGeneralTools::Tock_ms(t0);
-      //std::cout << "calc time was " << calc_time << std::endl;
-
-//      spworld.objects_.StepPhySimulation(DISCRETIZATION_STEP_SIZE);
-      spworld.gui_.Iterate(spworld.objects_);
-
-//    ninja_car.UpdateCarCommand(commandMSG);
-//    car.SetPose(posys_);
-//    spworld.gui_.Iterate(spworld.objects_);
-//    car.SetEngineMaxVel(50);
-//    car.SetFrontSteeringAngle(0);
-//    for(int ii=0; ii<20; ii++) {
-//        spworld.objects_.StepPhySimulation(0.1);
-//        spworld.gui_.Iterate(spworld.objects_);
-    }
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
-  }
+  std::this_thread::sleep_for(std::chrono::milliseconds(10));
   return 0;
 }
