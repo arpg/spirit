b84c809c6e1b480aa0c7439b663312bcc7b9bbad
bostoncleek <bocl8874@colorado.edu>
2018-09-04 14:45:05 -0600

    adding mesh functionality for extracting vertex data

--- include/spirit/Objects/spMesh.h
+++ include/spirit/Objects/spMesh.h
@@ -22,6 +22,12 @@ class spMesh : public spCommonObject {
   Eigen::MatrixXd GetVertices();
   Eigen::MatrixXd GetNormals();
 
+  Eigen::VectorXd localSearch(Eigen::MatrixXd vertex, double x, double y, double z, double r);
+  Eigen::VectorXd medianAngles(Eigen::MatrixXd normal, Eigen::VectorXd index);
+  double checkPitch(Eigen::VectorXd angles);
+  Eigen::VectorXd nearestVertex(Eigen::MatrixXd vertex, Eigen::VectorXd carPose);
+  Eigen::VectorXd checkPoint(Eigen::VectorXd v, Eigen::VectorXd carPose, double rw);
+
  private:
   spMeshSize dims_;
   spPose pose_;
